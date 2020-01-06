6a90a5ea1aa164cca857a41dca1a94b1c5dca706
Nicolas Wolf <ndwolf@gmail.com>
2020-01-06 19:02:50 -0300

    readded cors support for restful channel with origin configuration to enable it only on dev environments

--- channels/restful/app.py
+++ channels/restful/app.py
@@ -11,6 +11,7 @@ from bbot.config import load_configuration
 
 """Create and configure an instance of the application."""
 app = Flask(__name__)
+CORS(app)
 
 config = load_configuration(os.path.abspath(os.path.dirname(__file__) + "../../../instance"), "BBOT_ENV")
 app.config.from_mapping(config)
@@ -22,6 +23,7 @@ telegram = Plugin.load_plugin(config['channel_telegram'])
 print("Listening Telegram from path: " + telegram.get_webhook_path())
 
 @app.route(restful.get_endpoint_path(), methods=['POST'])
+@cross_origin(origins=restful.config['cors_origin'])
 def restful_endpoint(): # pylint: disable=W0612    
     response = restful.endpoint(request)
     return Response(response['response'], status=response['status'], mimetype=response['mimetype'])
